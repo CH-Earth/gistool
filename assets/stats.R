05b243dea3ef90dfaacc6e702e9c85d0f0248c25
Kasra Farmer <kasra.keshavarz@usask.ca>
2022-07-06 12:14:46 -0400

    adding license header

--- assets/stats.R
+++ assets/stats.R
@@ -1,5 +1,23 @@
+# Geospatial Dataset Processing Workflow
+# Copyright (C) 2022, University of Saskatchewan
+#
+# This file is part of the Geospatial Dataset Processing Workflow
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License 
+# along with this program.  If not, see <http://www.gnu.org/licenses/>. 
+
 # reading arguments
-args <- commandArgs(); 
+args <- commandArgs();
 
 # assigning variables to input arguments
 exactextractr_cache_path <- args[6]; 
@@ -29,3 +47,4 @@ q <- as.double(unlist(strsplit(quantiles, ",")));
 s <- unlist(strsplit(stats, ","));
 df <- cbind(p[[1]], exactextractr::exact_extract(r, p, s, quantiles=q)); # assuming first column indicates ID
 write.csv(df, output_path, row.names=FALSE, quote=FALSE)
+
